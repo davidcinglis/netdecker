0dc9654da0ee368935ced05cf3803ecd4d3e26ea
David Inglis <davidcinglis@gmail.com>
2021-11-29 16:21:56 -0800

    packaged code into a module to support pytest

---
+++ setup.py
@@ -0,0 +1,29 @@
+import setuptools
+import site
+import sys
+site.ENABLE_USER_SITE = "--user" in sys.argv[1:]
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="netdecker",
+    version="0.0.1",
+    author="David Inglis",
+    author_email="davidcinglis@gmail.com",
+    description="Creates importable decklists from MTG deck images.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/davidcinglis/decklist-ocr",
+    project_urls={
+        "Bug Tracker": "https://github.com/davidcinglis/decklist-ocr/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operation System :: OS Independent",
+    ],
+    package_dir={"": "netdecker"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+)
