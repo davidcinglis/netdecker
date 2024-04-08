c6ec12110ee8889c1ecb6509d9002a26ffa9f54a
David Inglis <davidcinglis@gmail.com>
2024-04-08 10:36:54 -0400

    lci/otj db update

--- netdecker/cardfile_data/database_setup.py
+++ netdecker/cardfile_data/database_setup.py
@@ -3,7 +3,7 @@ import sqlite3 as sl
 import formats
 
 def generate_card_records():
-    scryfall_url = "https://data.scryfall.io/oracle-cards/oracle-cards-20240117220308.json"
+    scryfall_url = "https://data.scryfall.io/oracle-cards/oracle-cards-20240408090157.json"
     card_dict = requests.get(scryfall_url).json()
     card_objects = []
     card_legalities = []
